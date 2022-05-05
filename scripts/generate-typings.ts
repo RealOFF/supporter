f0b07fa3cb056b3519a5151401c7332e4562fb26
RealOFF <gimpelllll@gmail.com>
2022-05-05 16:14:28 +0400

    Fix CI

--- scripts/generate-typings.ts
+++ scripts/generate-typings.ts
@@ -3,8 +3,10 @@ import { join } from 'path'
 
 const definitionsFactory = new GraphQLDefinitionsFactory()
 
+console.log('process.cwd()', process.cwd())
+
 definitionsFactory.generate({
-  typePaths: ['./src/**/*.graphql'],
-  path: join(process.cwd(), 'src/graphql.schema.ts'),
+  typePaths: [join(process.cwd(), 'apps/api/src/**/*.graphql')],
+  path: join(process.cwd(), '/apps/api/src/graphql.schema.ts'),
   outputAs: 'class'
 })
