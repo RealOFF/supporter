bb289c372bae6baa320f707ba3cde96ccffe79d5
mhimpel <someonewritecodehere@gmail.com>
2022-05-05 16:03:14 +0400

    Add base entities

---
+++ apps/api/src/app/users/users.resolver.ts
@@ -0,0 +1,38 @@
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql'
+
+import { UsersService } from './users.service'
+import { CreateUserInput } from '../../graphql.schema'
+import { UpdateUserInput } from '../../graphql.schema'
+
+@Resolver('User')
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Mutation('createUser')
+  create(@Args('createUserInput') createUserInput: CreateUserInput) {
+    return this.usersService.create(createUserInput)
+  }
+
+  @Query('users')
+  findAll() {
+    return this.usersService.findAll()
+  }
+
+  @Query('user')
+  findOne(@Args('id') id: number) {
+    return this.usersService.findOne({ id })
+  }
+
+  @Mutation('updateUser')
+  update(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
+    return this.usersService.update({
+      where: { id: updateUserInput.id },
+      data: updateUserInput
+    })
+  }
+
+  @Mutation('removeUser')
+  remove(@Args('id') id: number) {
+    return this.usersService.remove({ id })
+  }
+}
