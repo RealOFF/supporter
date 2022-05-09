d9966369521d0bb7e090908d26bcc9f4048f7a22
RealOFF <gimpelllll@gmail.com>
2022-05-10 00:30:13 +0400

    Add home prototype page

--- apps/web-client/pages/index.tsx
+++ apps/web-client/pages/index.tsx
@@ -1,11 +1,14 @@
+import Router from 'next/router'
+import { useEffect } from 'react'
 import styled from 'styled-components'
 
-const StyledPage = styled.div`
-  .page {
-  }
-`
+const StyledPage = styled.div``
 
 export function Index() {
+  useEffect(() => {
+    Router.push('/home')
+  }, [])
+
   return <StyledPage></StyledPage>
 }
 
