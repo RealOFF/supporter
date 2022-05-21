a4e36370ff67db4fa983a6852375bd104e977db7
RealOFF <gimpelllll@gmail.com>
2022-05-21 23:18:44 +0400

    Fix tests

--- apps/web-client/pages/index.tsx
+++ apps/web-client/pages/index.tsx
@@ -1,13 +1,15 @@
-import Router from 'next/router'
+import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import styled from 'styled-components'
 
 const StyledPage = styled.div``
 
 export function Index() {
+  const router = useRouter()
+
   useEffect(() => {
-    Router.push('/home')
-  }, [])
+    router.push('/home')
+  }, [router])
 
   return <StyledPage></StyledPage>
 }
